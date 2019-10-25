010f4f90fba168dfa0da784447967992963a2d82
MatthewCalligaro <matthewcalligaro@hotmail.com>
2019-10-24 21:00:05 -0700

    Created lab template

---
+++ labs/template/lab-template.py
@@ -0,0 +1,48 @@
+"""
+Copyright Harvey Mudd College
+Fall 2019
+
+Lab <n> - <Lab Title>
+"""
+
+################################################################################
+# Imports
+################################################################################
+
+import sys
+sys.path.insert(0, '../../library')
+from racecar_core import *
+rospy.init_node('racecar')
+
+
+################################################################################
+# Global variables
+################################################################################
+
+rc = Racecar()
+
+
+################################################################################
+# Functions
+################################################################################
+
+"""
+This function is run once every time the start button is pressed
+"""
+def start():
+    pass
+
+"""
+After start() is run, this function is run every frame until the back button is
+pressed
+"""
+def update():
+    pass
+
+
+################################################################################
+# Do not modify any code beyond this point
+################################################################################
+
+if __name__ == "__main__":
+    rc.run(start, update)
