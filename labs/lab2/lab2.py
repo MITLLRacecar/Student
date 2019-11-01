6ab995533fce6bad79dfce4679adc19833604f2c
edj2010 <evan.durston.johnson@gmail.com>
2019-10-31 18:25:01 -0700

    created lab 2

---
+++ labs/lab2/lab2.py
@@ -0,0 +1,49 @@
+"""
+Copyright Harvey Mudd College
+MIT License
+Fall 2019
+
+Lab <n> - <Lab Title>
+"""
+
+################################################################################
+# Imports
+################################################################################
+
+import sys
+sys.path.insert(0, '../../library')
+from racecar_core import *
+rospy.init_node('racecar')
+
+
+################################################################################
+# Global variables
+################################################################################
+
+rc = Racecar()
+
+
+################################################################################
+# Functions
+################################################################################
+
+def start():
+    """
+    This function is run once every time the start button is pressed
+    """
+    pass
+
+def update():
+    """
+    After start() is run, this function is run every frame until the back button
+    is pressed
+    """
+    pass
+
+
+################################################################################
+# Do not modify any code beyond this point
+################################################################################
+
+if __name__ == "__main__":
+    rc.set_start_update(start, update)
