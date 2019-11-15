89627afd2fa47a71bd0502a4c21ebf41e6f408dd
MatthewCalligaro <matthewcalligaro@hotmail.com>
2019-11-15 00:06:29 -0800

    Gampad no longer handles callbacks

--- imageModifications/gamepad.py
+++ imageModifications/gamepad.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 # node for turning gamepad inputs into drive commands
+# TODO: This file currently does nothing.  See if we can delete it. 
 
 import rospy
 from sensor_msgs.msg import Joy
@@ -13,9 +14,7 @@ def set_core_callback(callback):
     core_callback = callback
 
 def controller_callback(msg):
-    print("gamepad controller callback called")
-    if core_callback is not None:
-        core_callback(msg)
+    pass
 
 if __name__ == "__main__":
     # init ROS
