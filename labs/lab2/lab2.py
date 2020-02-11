45b4e85ae90828194c6edbf4b46d7e06170756d7
edj2010 <evan.durston.johnson@gmail.com>
2020-02-11 00:28:37 -0500

    add lab2 base code

--- labs/lab2/lab2.py
+++ labs/lab2/lab2.py
@@ -3,7 +3,7 @@ Copyright Harvey Mudd College
 MIT License
 Fall 2019
 
-Lab <n> - <Lab Title>
+Lab 2 - Line Following
 """
 
 ################################################################################
@@ -27,17 +27,41 @@ rc = Racecar()
 # Functions
 ################################################################################
 
+def crop(image, top_left, bottom_right):
+    '''
+    Helper function to make cropping images easier
+    '''
+    pass
+
+def get_mask(image, hsv_low, hsv_high):
+    '''
+    Helper function to get the image mask given an HSV range
+    '''
+    pass
+
+def get_contour(mask):
+    '''
+    Get a contour from the mask we created
+    '''
+    pass
+
+def find_center(contour):
+    '''
+    Compute the coordinates of the center of our chosen contour
+    '''
+    pass
+
 def start():
-    """
+    '''
     This function is run once every time the start button is pressed
-    """
+    '''
     pass
 
 def update():
-    """
+    '''
     After start() is run, this function is run every frame until the back button
     is pressed
-    """
+    '''
     pass
 
 
