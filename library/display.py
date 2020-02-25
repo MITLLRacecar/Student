7606f7fc30414bea28f0446e01d2ea6ce2f7da84
MatthewCalligaro <MatthewCalligaro@hotmail.com>
2020-02-24 19:56:01 -0800

    began work on display module

---
+++ library/display.py
@@ -0,0 +1,23 @@
+"""
+Copyright Harvey Mudd College
+MIT License
+Spring 2020
+
+Contains the Display module of the racecar_core library
+"""
+
+import cv2 as cv
+
+class Display:
+    """
+    Class docstring
+    """
+
+    def __init__(self):
+        pass
+
+    def show_image(self, image):
+        cv.imshow("display", image)
+
+    def show_text(self, text, size, color):
+        pass
