7324dab8ddf101477ea39d6b1f16331c51fc9e81
MatthewCalligaro <MatthewCalligaro@hotmail.com>
2020-03-03 18:43:18 -0800

    Add image displaying capabilites, small fixes

--- labs/lab2/lab2_starter.py
+++ labs/lab2/lab2_starter.py
@@ -30,13 +30,11 @@ rc = Racecar()
 # The smallest contour we will recognize as a valid contour
 MIN_CONTOUR_SIZE = 30
 # A crop window for the floor directly in front of the car
-CROP_BOTTOM = ((400, 0), (479, 639))
+CROP_BOTTOM = ((400, 0), (rc.camera.get_height(), rc.camera.get_width()))
 
 # Variables
 speed = 0.0  # The current speed of the car
 angle = 0.0  # The current angle of the car's wheels
-center = (0, 0)  # The (row, column) of the center pixel of the line
-area = 0  # The number of pixels in the largest contour
 
 # Colors, stored as a pair (hsv_min, hsv_max)
 BLUE = ((90, 50, 50), (110, 255, 255))  # The HSV range for the color blue
@@ -48,18 +46,22 @@ BLUE = ((90, 50, 50), (110, 255, 255))  # The HSV range for the color blue
 ################################################################################
 
 
-def crop(image, top_left, bottom_right):
+def crop(image, top_left_inclusive, bottom_right_exclusive):
     """
     Crops an image to a rectangle based on the specified pixel points
 
     Inputs:
-        image (2D numpy array of tripples): The image to crop
-        top_left ((int, int)): The (row, column) of the top left pixel of the
-            crop rectangle
-        bottom_right((int, int)): The (row, column) of the bottom right pixel
+        image (2D numpy array of triples): The image to crop
+        top_left_inclusive ((int, int)): The (row, column) of the top left pixel
             of the crop rectangle
+        bottom_right_exclusive ((int, int)): The (row, column) of the pixel one
+            past the bottom right corner of the crop rectangle
 
-    Output (2D numpy array of tripples): a cropped version of the image
+    Note: The top_left_inclusive pixel is included in the crop rectangle, but
+        the bottom_right_exclusive pixel is not.  This is similar to how the how
+        range(1, 4) returns [1, 2, 3] in Python.
+
+    Output (2D numpy array of triples): a cropped version of the image
 
     Example:
     ```Python
@@ -67,14 +69,13 @@ def crop(image, top_left, bottom_right):
 
     # Crop the image to only keep the top half
     cropped_image = crop(
-        image, (0, 0), (rc.camera.get_height() - 1, rc.camera.get_width() - 1)
+        image, (0, 0), (rc.camera.get_height() / 2, rc.camera.get_width())
     )
     ```
     """
     # Extract the minimum and maximum pixel rows and columns from the parameters
-    r_min, c_min = top_left
-    r_max = bottom_right[0] + 1
-    c_max = bottom_right[1] + 1
+    r_min, c_min = top_left_inclusive
+    r_max, c_max = bottom_right_exclusive
 
     # Shorten the array to the specified row and column ranges
     return image[r_min:r_max, c_min:c_max]
@@ -85,7 +86,7 @@ def find_contours(image, hsv_lower, hsv_upper):
     Finds all contours of the specified color range in the provided image
 
     Inputs:
-        image (2D numpy array of tripples): The image in which to find contours,
+        image (2D numpy array of triples): The image in which to find contours,
             with pixels represented in the bgr (blue-green-red) format
         hsv_lower ((int, int, int)): The lower bound for the hue, saturation,
             and value of colors to contour
@@ -153,11 +154,27 @@ def get_largest_contour(contours):
     return greatest_contour
 
 
+def draw_contour(image, contour, color=(0, 255, 0)):
+    """
+    Draws a contour on a copy of the provided image
+
+    Inputs:
+        image (2D numpy array of triples): The image on which to draw the
+            contour
+        contour (contour): A contour to draw on the image
+        color ((int, int, int)): The color to draw the contour, specified as
+            blue-green-red channels each ranging from 0 to 255
+
+    Output (2D numpy array ): a copy of image with the contour drawn on it
+    """
+    return cv.drawContours(np.copy(image), [contour], 0, color, 3)
+
+
 def get_center(contour):
     """
     Finds the center of a contour from an image
 
-    Input:
+    Inputs:
         contour (contour): The contour of which to find the center
 
     Output ((int, int)): The (row, column) of the pixel at the center of the 
@@ -197,7 +214,7 @@ def get_area(contour):
     """
     Finds the area of a contour from an image
 
-    Input:
+    Inputs:
         contour (contour): The contour of which to measure the area
 
     Output (float): The number of pixels contained within the contour, or 0 if
@@ -228,14 +245,10 @@ def start():
     """
     global speed
     global angle
-    global center
-    global area
 
     # Initialize variables
     speed = 0
     angle = 0
-    center = None
-    area = 0
 
     # Set initial driving speed and angle
     rc.drive.set_speed_angle(speed, angle)
@@ -261,17 +274,17 @@ def update():
     """
     global speed
     global angle
-    global center
-    global area
 
     image = rc.camera.get_image()
+    center = None
+    area = 0
 
     # TODO: Implement a way to cycle through following multiple colors of tape
 
     if image is None:
         # If no image is found, center the wheels
-        center = None
         angle = 0
+        center = None
         area = 0
     else:
         # Crop the image to only show the floor in front of the car
@@ -308,7 +321,7 @@ def update():
         if center is None:
             print("No contour found")
         else:
-            print("Center:", center, "Area:", angle)
+            print("Center:", center, "Area:", area)
 
 
 def update_slow():
@@ -316,22 +329,43 @@ def update_slow():
     After start() is run, this function is run at a constant rate that is slower
     than update().  By default, update_slow() is run once per second
     """
-    # Print a line of ascii text denoting where the car sees the line and the
-    # area of the contour
+    # To help debug, update_slow does the following:
+    # 1. Prints a line of ascii text to the console denoting the area of the
+    #    contour and where the car sees the line
+    # 2. Shows the current image to the screen with the largest contour drawn
+    #    on top in bright green
 
-    # If no image is found, print all X's
-    if rc.camera.get_image() is None:
-        print("X" * 10 + " (No image) " + "X" * 10 + " : area = " + str(area))
-
-    # If an image is found but no contour is found, print all dashes
-    elif center is None:
-        print("-" * 32 + " : area = " + str(area))
-
-    # Otherwise, print a line of dashes with a | where the line is seen
+    image = rc.camera.get_image()
+    if image is None:
+        # If no image is found, print all X's and don't display an image
+        print("X" * 10 + " (No image) " + "X" * 10)
     else:
-        s = ["-"] * 32
-        s[int(center[1] / 20)] = "|"
-        print("".join(s) + " : area = " + str(area))
+        # Crop the image to only show the floor in front of the car
+        cropped_image = crop(image, CROP_BOTTOM[0], CROP_BOTTOM[1])
+
+        # Find all of the blue contours
+        blueContours = find_contours(cropped_image, BLUE[0], BLUE[1])
+
+        # Find the center and area of the largest blue contour
+        largestBlueContour = get_largest_contour(blueContours)
+        center = get_center(largestBlueContour)
+        area = get_area(largestBlueContour)
+
+        # If an image is found but no contour is found, print all dashes
+        if center is None:
+            print("-" * 32 + " : area = " + str(area))
+
+        # Otherwise, print a line of dashes with a | where the line is seen
+        else:
+            s = ["-"] * 32
+            s[int(center[1] / 20)] = "|"
+            print("".join(s) + " : area = " + str(area))
+
+            # Draw the contour onto the image
+            cropped_image = draw_contour(cropped_image, largestBlueContour)
+
+        # Display the image to the screen
+        rc.display.show_image(cropped_image)
 
 
 ################################################################################
--- library/display.py
+++ library/display.py
@@ -8,17 +8,31 @@ Contains the Display module of the racecar_core library
 
 import cv2 as cv
 
+
 class Display:
     """
-    Class docstring
+    Allows the user to print text and images to the RACECAR screen
     """
 
     def __init__(self):
         pass
 
     def show_image(self, image):
+        """
+        Displays an image on a window of the RACECAR desktop
+
+        Inputs:
+            image: (2D numpy array of triples): The image to display to the
+                screen encoded as a 2D array of pixels, where each pixel is
+                stored as a (blue, green, red) triple
+
+        Example:
+        ```Python
+        image = rc.camera.get_image();
+
+        # Show the image captured by the camera
+        rc.display.show_image(image);
+        ```
+        """
         cv.imshow("display window", image)
         cv.waitKey(1)
-
-    def show_text(self, text, size, color):
-        pass
