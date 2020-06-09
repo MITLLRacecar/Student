0ceb8158051b105a38c0b566775a2c427615d2e5
MatthewCalligaro <MatthewCalligaro@hotmail.com>
2020-06-09 16:20:42 -0700

    verified waitKey is necessary

--- library/simulation/display_sim.py
+++ library/simulation/display_sim.py
@@ -10,4 +10,4 @@ class DisplaySim(Display):
     def show_color_image(self, image):
         cv.namedWindow(self.__WINDOW_NAME, cv.WINDOW_NORMAL)
         cv.imshow(self.__WINDOW_NAME, image)
-        cv.waitKey(1)  # TODO is this line necessary?
+        cv.waitKey(1)
