60b361e014fd1d47f73288a743a1144ff7a88acb
MatthewCalligaro <MatthewCalligaro@hotmail.com>
2020-06-15 10:59:38 -0700

    Created lab2b (cone parking)

---
+++ labs/lab2/lab2b.py
@@ -0,0 +1,163 @@
+"""
+Copyright Harvey Mudd College
+MIT License
+Fall 2019
+
+Lab 2 - Image Processing
+"""
+
+########################################################################################
+# Imports
+########################################################################################
+
+import sys
+import cv2 as cv
+import numpy as np
+
+sys.path.insert(1, "../../library")
+from racecar_core import rc
+import racecar_utils as rc_utils
+
+########################################################################################
+# Global variables
+########################################################################################
+
+# Constants
+# The smallest contour we will recognize as a valid contour
+MIN_CONTOUR_SIZE = 30
+
+# Variables
+speed = 0.0  # The current speed of the car
+angle = 0.0  # The current angle of the car's wheels
+contour_center = None  # The (pixel row, pixel column) of contour
+contour_area = 0  # The area of contour
+
+# Colors, stored as a pair (hsv_min, hsv_max)
+ORANGE = ((10, 50, 50), (25, 255, 255))  # The HSV range for the color orange
+
+########################################################################################
+# Functions
+########################################################################################
+
+
+def update_contour():
+    """
+    Identifies a contour in the provided image to update global variables.
+
+    Args:
+        image: (2D numpy array of pixels) The image in which to find contours.
+
+    Note:
+        Updates the global variables contour, contour_image, contour_center,
+        and contour_area.
+    """
+    global contour_center
+    global contour_area
+
+    image = rc.camera.get_color_image()
+
+    if image is None:
+        contour_center = None
+        contour_area = 0
+    else:
+        # Find all of the blue contours
+        contours = rc_utils.find_contours(image, ORANGE[0], ORANGE[1])
+
+        # Select the largest contour
+        contour = rc_utils.get_largest_contour(contours, MIN_CONTOUR_SIZE)
+
+        if contour is not None:
+            # Calculate contour information
+            contour_center = rc_utils.get_contour_center(contour)
+            contour_area = rc_utils.get_contour_area(contour)
+
+            # Draw contour onto the image
+            rc_utils.draw_contour(image, contour)
+            rc_utils.draw_circle(image, contour_center)
+
+        # Display the image to the screen
+        rc.display.show_color_image(image)
+
+
+def start():
+    """
+    This function is run once every time the start button is pressed
+    """
+    global speed
+    global angle
+
+    # Initialize variables
+    speed = 0
+    angle = 0
+
+    # Set initial driving speed and angle
+    rc.drive.set_speed_angle(speed, angle)
+
+    # Set update_slow to refresh every half second
+    rc.set_update_slow_time(0.5)
+
+    # Print start message
+    print(
+        ">> Lab 2 - Image processing\n"
+        "\n"
+        "Controlls:\n"
+        "   Right trigger = accelerate forward\n"
+        "   Left trigger = accelerate backward\n"
+        "   A button = print current speed and angle\n"
+        "   B button = print contour center and area"
+    )
+
+
+def update():
+    """
+    After start() is run, this function is run every frame until the back button
+    is pressed
+    """
+    global speed
+    global angle
+
+    # Search for contours in the current color image
+    update_contour()
+
+    # TODO: Have the car drive up to the cone and park 1 meter away
+
+    # Print the current speed and angle when the A button is held down
+    if rc.controller.is_down(rc.controller.Button.A):
+        print("Speed:", speed, "Angle:", angle)
+
+    # Print the center and area of the largest contour when B is held down
+    if rc.controller.is_down(rc.controller.Button.B):
+        if contour_center is None:
+            print("No contour found")
+        else:
+            print("Center:", contour_center, "Area:", contour_area)
+
+
+def update_slow():
+    """
+    After start() is run, this function is run at a constant rate that is slower
+    than update().  By default, update_slow() is run once per second
+    """
+    # Print a line of ascii text denoting the contour area and x position
+    if rc.camera.get_color_image() is None:
+        # If no image is found, print all X's and don't display an image
+        print("X" * 10 + " (No image) " + "X" * 10)
+    else:
+        # If an image is found but no contour is found, print all dashes
+        if contour_center is None:
+            print("-" * 32 + " : area = " + str(contour_area))
+
+        # Otherwise, print a line of dashes with a | indicating the contour x-position
+        else:
+            s = ["-"] * 32
+            s[int(contour_center[1] / 20)] = "|"
+            print("".join(s) + " : area = " + str(contour_area))
+
+
+########################################################################################
+# DO NOT MODIFY: Register start and update and begin execution
+########################################################################################
+
+if __name__ == "__main__":
+    rc.set_start_update(start, update, update_slow)
+    rc.go()
