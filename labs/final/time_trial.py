b6768dca9bacba1c22895aa5183e2ec7b170dd14
MatthewCalligaro <MatthewCalligaro@hotmail.com>
2020-11-01 22:49:22 -0800

    Create final challenge starter code

---
+++ labs/final/time_trial.py
@@ -0,0 +1,59 @@
+"""
+Copyright MIT and Harvey Mudd College
+MIT License
+Fall 2020
+
+Final Challenge - Time Trial
+"""
+
+########################################################################################
+# Imports
+########################################################################################
+
+import sys
+import cv2 as cv
+import numpy as np
+
+sys.path.insert(0, "../../library")
+import racecar_core
+import racecar_utils as rc_utils
+
+########################################################################################
+# Global variables
+########################################################################################
+
+rc = racecar_core.create_racecar()
+
+# Add any global variables here
+
+########################################################################################
+# Functions
+########################################################################################
+
+
+def start():
+    """
+    This function is run once every time the start button is pressed
+    """
+    # Have the car begin at a stop
+    rc.drive.stop()
+
+    # Print start message
+    print(">> Final Challenge - Time Trial")
+
+
+def update():
+    """
+    After start() is run, this function is run every frame until the back button
+    is pressed
+    """
+    pass
+
+
+########################################################################################
+# DO NOT MODIFY: Register start and update and begin execution
+########################################################################################
+
+if __name__ == "__main__":
+    rc.set_start_update(start, update, None)
+    rc.go()
