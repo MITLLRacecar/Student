105f180ca95306b9fa946d2a70827d3d349e8adb
ArjunGupte1 <43100142+ArjunGupte1@users.noreply.github.com>
2022-07-23 00:47:38 -0700

    Updated drone_sim.py with feedback from PR

--- library/simulation/drone_sim.py
+++ library/simulation/drone_sim.py
@@ -12,38 +12,19 @@ class DroneSim(Drone):
         self.__drone_image: NDArray[(480, 640, 3), np.uint8] = None
         self.__is_drone_image_current: bool = False
 
-    def get_drone_image_no_copy(self) -> NDArray[(480, 640, 3), np.uint8]:
+    def get_image_no_copy(self) -> NDArray[(480, 640, 3), np.uint8]:
         if not self.__is_drone_image_current:
             self.__drone_image = self.__request_drone_image(False)
             self.__is_drone_image_current = True
 
         return self.__drone_image
 
-    def get_drone_image_async(self) -> NDArray[(480, 640, 3), np.uint8]:
+    def get_image_async(self) -> NDArray[(480, 640, 3), np.uint8]:
         return self.__request_drone_image(True)
 
-    def __update(self) -> None:
-        self.__is_drone_image_current = False
-
-    def __request_drone_image(self, isAsync: bool) -> NDArray[(480, 640), np.uint8]:
-        # Ask for the current color image
+    def get_height(self) -> float:
         self.__racecar._RacecarSim__send_header(
-            self.__racecar.Header.drone_get_drone_image, isAsync
-        )
-
-        # Read the color image as 32 packets
-        raw_bytes = self.__racecar._RacecarSim__receive_fragmented(
-            32, self._WIDTH * self._HEIGHT * 4, isAsync
-        )
-        drone_image = np.frombuffer(raw_bytes, dtype=np.uint8)
-        drone_image = np.reshape(drone_image, (self._HEIGHT, self._WIDTH, 4), "C")
-
-        drone_image = cv.cvtColor(drone_image, cv.COLOR_RGB2BGR)
-        return drone_image
-
-    def get_drone_height(self) -> float:
-        self.__racecar._RacecarSim__send_header(
-            self.__racecar.Header.drone_get_drone_height
+            self.__racecar.Header.drone_get_height
         )
         value = struct.unpack("f", self.__racecar._RacecarSim__receive_data())[0]
         return value
@@ -55,7 +36,26 @@ class DroneSim(Drone):
             )
         )
 
-    def return_home(self) -> None:
+    def return_to_car(self) -> None:
         self.__racecar._RacecarSim__send_header(
-            self.__racecar.Header.drone_return_home
+            self.__racecar.Header.drone_return_to_car
         )
+
+    def __update(self) -> None:
+        self.__is_drone_image_current = False
+
+    def __request_drone_image(self, isAsync: bool) -> NDArray[(480, 640), np.uint8]:
+        # Ask for the current color image
+        self.__racecar._RacecarSim__send_header(
+            self.__racecar.Header.drone_get_image, isAsync
+        )
+
+        # Read the color image as 32 packets
+        raw_bytes = self.__racecar._RacecarSim__receive_fragmented(
+            32, self._WIDTH * self._HEIGHT * 4, isAsync
+        )
+        drone_image = np.frombuffer(raw_bytes, dtype=np.uint8)
+        drone_image = np.reshape(drone_image, (self._HEIGHT, self._WIDTH, 4), "C")
+
+        drone_image = cv.cvtColor(drone_image, cv.COLOR_RGB2BGR)
+        return drone_image
